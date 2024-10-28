a4fb372577195edab9ee602775f281794b6b0ca9
Jess Todd <jessicacktodd@gmail.com>
2024-10-28 12:33:40 +0000

    font working - not favicon though

---
+++ src/pages/_app.js
@@ -0,0 +1,16 @@
+import '../app/global.css';
+import Head from 'next/head';
+
+function MyApp({ Component, pageProps }) {
+    return (
+        <>
+            <Head>
+            <link rel="shortcut icon" href="/favicon.ico" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
+}
+
+export default MyApp;
+
